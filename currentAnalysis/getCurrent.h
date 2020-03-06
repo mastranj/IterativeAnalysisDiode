018716106b9946a7c1bdbf472ea00547eec212be
James A Mastran <jam2454@columbia.edu>
2020-03-06 14:26:12 -0500

    Added a header file and removed unnecessary includes from getCurrent.c

---
+++ currentAnalysis/getCurrent.h
@@ -0,0 +1,6 @@
+#ifndef GET_CURRENT
+#define GET_CURRENT
+double getCoeff(double, double, double, int,  int);
+double getVoltage(double, double, int, double, int);
+double getCurrent(double, double, double, int);
+#endif
